c70c5a48c1d3ca42d94d3fa1e9dbfab0be6993a8
ANSH3LL <53097224+ANSH3LL@users.noreply.github.com>
2021-04-20 21:00:40 +0300

    Initial commit

---
+++ picture.js
@@ -0,0 +1,70 @@
+//smihub
+const smihub = 'https://smihub.com/';
+const postPrefixS = 'https://smihub.com/c/';
+const profilePrefixS = 'https://smihub.com/v/';
+//picuki
+const picuki = 'https://www.picuki.com/';
+const postPrefixP = 'https://www.picuki.com/media/';
+const profilePrefixP = 'https://www.picuki.com/profile/';
+//regex
+const postRegex = /(?<!^https\:\/\/www\.google\.com\/imgres\?imgurl.*)(?:https?\:\/\/)?(?:www\.)?(instagram\.com|instagr\.am|next\=)(?:\/p|\/reel)\/([a-zA-Z._0-9-]+)/i;
+const profileRegex = /(?<!^https\:\/\/www\.google\.com\/imgres\?imgurl.*)(?:https?\:\/\/)?(?:www\.)?(?<!help\.|api\.|business\.|about\.|lookaside\.)(instagram\.com|instagr\.am|next\=)\/(?!accounts|explore|developer|reel)([a-zA-Z._0-9]{3,})/i;
+
+var filter = {urls: ["*://*.instagram.com/*"]};
+
+function getUsername(igurl) {
+    url = decodeURIComponent(igurl);
+    return url.match(profileRegex)[2];
+}
+
+function getMediaId(igurl) {
+    mediaId = BigInt(0);
+    url = decodeURIComponent(igurl);
+    shortcode = url.match(instaPost)[2].split('');
+    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
+    shortcode.forEach((letter) => {
+        mediaId = BigInt((mediaId * 64n)) + BigInt(alphabet.indexOf(letter));
+    });
+    return mediaId.toString().split('').reverse().join('');
+}
+
+function setPrefix(result) {
+    prefix = result.handler;
+    if(prefix == 1 || prefix == 2) {
+        document.igRequestHandler = prefix;
+        if(!browser.webRequest.onBeforeRequest.hasListener(igRedirect)) {
+            browser.webRequest.onBeforeRequest.addListener(igRedirect, filter, ['blocking']);
+        }
+    } else if(browser.webRequest.onBeforeRequest.hasListener(igRedirect)) {
+        browser.webRequest.onBeforeRequest.removeListener(igRedirect);
+    }
+}
+
+function loadPreferences(changes, areaName) {
+    pref = browser.storage.local.get('handler');
+    pref.then((result) => {setPrefix(result);}, (error) => {});
+}
+
+function igRedirect(details) {
+    if(document.igRequestHandler == 1) {
+        redirectTo = smihub;
+        postPrefix = postPrefixS;
+        profilePrefix = profilePrefixS;
+    } else if(document.igRequestHandler == 2) {
+        redirectTo = picuki;
+        postPrefix = postPrefixP;
+        profilePrefix = profilePrefixP;
+    }
+    //
+    if(postRegex.test(decodeURIComponent(details.url))) {
+        url = postPrefix + getMediaId(details.url);
+    } else if(profileRegex.test(decodeURIComponent(details.url))) {
+        url = profilePrefix + getUsername(details.url);
+    } else {
+        return {redirectUrl: redirectTo};
+    }
+    return {redirectUrl: url};
+}
+
+browser.storage.onChanged.addListener(loadPreferences);
+browser.webRequest.onBeforeRequest.addListener(igRedirect, filter, ['blocking']);
---
+++ popup/options.js
@@ -0,0 +1,43 @@
+function savePreferences(pref) {
+    browser.storage.local.set({handler: pref});
+}
+
+function loadPreferences() {
+    pref = browser.storage.local.get('handler');
+    pref.then((result) => {updateUi(result);}, (error) => {displayError();});
+}
+
+function updateUi(result) {
+    pref = result.handler;
+    if(pref == 1) {
+        document.getElementById('status').innerHTML = 'Status: Using smihub.com';
+    } else if(pref == 2) {
+        document.getElementById('status').innerHTML = 'Status: Using picuki.com';
+    } else {
+        document.getElementById('status').innerHTML = 'Status: Disabled';
+    }
+}
+
+function displayError() {
+    document.getElementById('status').innerHTML = 'Status: Error resolving handler';
+}
+
+function setSmihub() {
+    savePreferences(1);
+    loadPreferences();
+}
+
+function setPicuki() {
+    savePreferences(2);
+    loadPreferences();
+}
+
+function setDisabled() {
+    savePreferences(3);
+    loadPreferences();
+}
+
+document.addEventListener('DOMContentLoaded', loadPreferences);
+document.getElementById('smihub').addEventListener('click', setSmihub);
+document.getElementById('picuki').addEventListener('click', setPicuki);
+document.getElementById('disabled').addEventListener('click', setDisabled);
